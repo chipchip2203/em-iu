0bb96e5f6ef98c6195aeb0ccc38cbf00643611d5
KhietVy <hipdegai@gmail.com>
2025-03-24 07:19:01 -0500

    Add files via upload

---
+++ Crush-main/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Gửi em yêu",
+    introTitle: 'Chú chéo a kai chích của anh<3',
+    introDesc: `Quào !! Chúc mừng ngày anni lần thứ 21 của anh và bé`,
+    btnIntro: 'Vợ bấm đây nè',
+    title: 'Xin lỗi em vì sinh nhật vừa qua đã làm cho em khó chịu và buồn bực trong người.Nhưng mà anh và em luôn cố gằng thêm 1 chút vì nhau để có thể yêu nhau lâu hơn và trọn vẹn hơn từng ngày.Anh có ngàn lý do để yêu em từ những điều nhỏ nhặt nhất từ em như là cách mà anh yêu thương anh và hành động vì anh trong từng chút một , em luôn là hình mẫu của nữa kia mà anh luôn tìm kiếm ,cái cách mà em nhìn anh cũng đủ khiến anh biết rằng mình đã yêu đúng cô gái siêu giá trị rồi 🥰',
+    desc: 'Còn lý do em yêu anh là gì ?🤔🤔!!!',
+    btnYes: 'Do ku bự =))',
+    btnNo: 'vì anh cute dthw , đẹp người , đẹp nết , luôn làm cho em vui , biết cách chiếu chuộng em , yêu thương em hết mực ,Anh là gu của em...',
+    question: 'Anh biết là bé bi yêu anh nhiều lắm và vô cùng tích cực , anh chỉ đùa em thôi em đừng có khó chịu nhaaa🫶🫶',
+    btnReply: 'Gửi cho anh điiiii',
+    reply: 'không sao đâu bb yêu em cũng mê sếch với anh lắm a hihihiihihihihihiihihihihihihihihhihihihhihihihihhihihihihihihihihihihihihihihihihihihiihihihihihihihi',
+    mess: 'anh biết mà . Yêu Vy nhiều nhiều 😘😘',
+    messDesc: 'Yêu mỗi em , mê mỗi em . chịch mỗi emmm.',
+    btnAccept: 'hà hà hà',
+    messLink: 'https://youtube.com/shorts/LbUlB6saNRA?si=0X-kRRRvvoQoKgOE' 
+}
